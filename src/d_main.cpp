26283a8a8f85b611af5bcd8c0d38a6a0a308d36b
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2022-07-13 14:16:41 -0400

    - always print GZDoom version in the startup log

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -3570,7 +3570,7 @@ static int D_DoomMain_Internal (void)
 		Printf("\n");
 	}
 
-	if (!batchrun) Printf(PRINT_LOG, "%s version %s\n", GAMENAME, GetVersionString());
+	Printf("%s version %s\n", GAMENAME, GetVersionString());
 
 	D_DoomInit();
 
