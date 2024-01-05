8964d00ba1793a5492652cf8363fd8ddba7125cd
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2024-01-04 19:20:50 -0500

    - use separate .exe and .pk3 name to allow multi-distribution of two different engines on Steam

--- src/version.h
+++ src/version.h
@@ -98,7 +98,7 @@ const char *GetVersionString();
 
 // This is so that derivates can use the same savegame versions without worrying about engine compatibility
 #define GAMESIG "HANDSOFNECROMANCY"
-#define BASEWAD "handsofnecromancy.pk3"
+#define BASEWAD "handsofnecromancy-vkdoom.pk3"
 #define OPTIONALWAD "game_support.pk3"
 #define GZDOOM 1
 #define VR3D_ENABLED
