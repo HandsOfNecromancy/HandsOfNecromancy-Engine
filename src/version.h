015f9293c085129a35fb211e97739e0c3e72f8ed
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2025-02-13 15:28:17 -0500

    - don't load gzdoom's game_support.pk3

--- src/version.h
+++ src/version.h
@@ -94,7 +94,7 @@ const char *GetVersionString();
 #define GAMESIG "HANDSOFNECROMANCY"
 #define BASEWAD "engine-classic.pk3"
 // Set OPTIONALWAD to "" (null) to disable searching for it
-#define OPTIONALWAD "game_support.pk3"
+#define OPTIONALWAD "hon_support.pk3"
 #define GZDOOM 1
 #define VR3D_ENABLED
 
